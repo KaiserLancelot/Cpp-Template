f2d4107b2294b6dda86653d0875e818711eeb514
kaiser <1244713586@qq.com>
2020-10-08 18:59:05 +0800

    2020.10.8

--- scripts/install-system.sh
+++ scripts/install-system.sh
@@ -5,12 +5,7 @@ set -e
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
     sudo apt update
-    sudo apt install gcc-10 g++-10 llvm valgrind
-
-    # FIXME clang-tidy failed to install using apt
-    wget -q https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.1/clang+llvm-10.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-    tar -xf clang+llvm-10.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-    sudo ln clang+llvm-10.0.1-x86_64-linux-gnu-ubuntu-16.04/bin/clang-tidy /usr/bin/clang-tidy
+    sudo apt install gcc-10 g++-10 llvm clang-tidy valgrind
 
     export CC=gcc-10
     export CXX=g++-10
